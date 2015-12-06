b1e69f4b8c7e52130c9005e660fc7cb55ae97ba4
Edward <edward.huang03@gmail.com>
2015-12-06 17:04:53 -0500

    added glulookat

---
+++ src/globals.h
@@ -0,0 +1,27 @@
+/*
+ * globals.h
+ *
+ *  Created on: 2015 M12 6
+ *      Author: huan9380
+ */
+
+#ifndef SRC_GLOBALS_H_
+#define SRC_GLOBALS_H_
+
+// Math Constants
+const float PI = 3.1415926535;
+const float TO_RADIANS = PI/180.0;
+
+//Display Camera Constants
+const float maximumViewerDistance    = 20;
+const float minimumViewerDistance    = 0;
+const float initialViewerDistance    = 10;
+const float viewerDistanceIncrement  = 0.1;
+const float initialViewerAzimuth     = 0.0;
+const float initialViewerZenith      = PI / 2.0;
+const float viewerAngleIncrement     = PI / 60.0;
+const float lookAtPosition[] = { 0.0, 0.0, 0.0 };
+
+
+
+#endif /* SRC_GLOBALS_H_ */
